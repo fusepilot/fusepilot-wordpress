9e26fa169350b49c3170e44bda26421ce1f56e1c
Michael Delaney <fusepilot@gmail.com>
2012-01-13 10:15:33 -0600

    add search icon. cleaned search templates.

--- wp-content/themes/fusepilot/partials/search_form.php
+++ wp-content/themes/fusepilot/partials/search_form.php
@@ -1,6 +1,9 @@
 <section id="search">
   <!-- <h3>Search</h3> -->
   
-  <?php get_search_form(); ?>
+  <form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
+    <input type="text" id="s" name="s" value="" placeholder="Enter search terms" />
+    <input type="submit" value="Search" id="searchsubmit" />
+  </form>
 </section>
 
--- wp-content/themes/fusepilot/searchform.php
+++
@@ -1,4 +0,0 @@
-<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
-  <input type="text" id="s" name="s" value="" placeholder="Enter search terms" />
-  <input type="submit" value="Search" id="searchsubmit" />
-</form>
