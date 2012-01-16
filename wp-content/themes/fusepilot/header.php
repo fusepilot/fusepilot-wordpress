c58afef90896656191c03d44d144793cdf5d347f
Michael Delaney <fusepilot@gmail.com>
2012-01-16 15:33:26 -0600

    cleaned up header.

--- wp-content/themes/fusepilot/header.php
+++ wp-content/themes/fusepilot/header.php
@@ -7,11 +7,9 @@
 <!--[if gt IE 9]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
 <!-- the "no-js" class is for Modernizr. -->
 
-<head id="www-sitename-com" data-template-set="html5-reset-wordpress-theme" profile="http://gmpg.org/xfn/11">
+<head profile="http://gmpg.org/xfn/11">
 
 	<meta charset="<?php bloginfo('charset'); ?>">
-	
-	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	
 	<?php if (is_search()) { ?>
@@ -26,13 +24,8 @@
 	<meta name="google-site-verification" content="">
 	<!-- Speaking of Google, don't forget to set your site up: http://google.com/webmasters -->
 	
-	<meta name="author" content="Your Name Here">
-	<meta name="Copyright" content="Copyright Your Name Here 2011. All Rights Reserved.">
-
-	<!-- Dublin Core Metadata : http://dublincore.org/ -->
-	<meta name="DC.title" content="Project Name">
-	<meta name="DC.subject" content="What you're about.">
-	<meta name="DC.creator" content="Who made this site.">
+	<meta name="author" content="Michael Delaney">
+	<meta name="Copyright" content="Copyright Fusepilot 2011. All Rights Reserved.">
 	
 	<!--  Mobile Viewport meta tag
 	j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
@@ -44,50 +37,19 @@
 	-->
 	
 	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico">
-	<!-- This is the traditional favicon.
-		 - size: 16x16 or 32x32
-		 - transparency is OK
-		 - see wikipedia for info on browser support: http://mky.be/favicon/ -->
-		 
 	<link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/images/apple-touch-icon.png">
-	<!-- The is the icon for iOS's Web Clip.
-		 - size: 57x57 for older iPhones, 72x72 for iPads, 114x114 for iPhone4's retina display (IMHO, just go ahead and use the biggest one)
-		 - To prevent iOS from applying its styles to the icon name it thusly: apple-touch-icon-precomposed.png
-		 - Transparency is not recommended (iOS will put a black BG behind the icon) -->
-	
-	<!-- CSS: screen, mobile & print are all in the same file -->
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	
 	<script src="<?php bloginfo('template_directory'); ?>/_/js/jquery-1.7.1.min.js"></script>
-	<!-- all our JS is at the bottom of the page, except for Modernizr. -->
 	<script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-1.7.min.js"></script>
-	
 	<script src="<?php bloginfo('template_directory'); ?>/_/js/cufon-yui.js"></script>
-	
-	<!--<script src="<?php bloginfo('template_directory'); ?>/_/js/forza_325.font.js"></script>-->
 	<script src="<?php bloginfo('template_directory'); ?>/_/js/Forza_Light_300-Forza_Medium_350-Forza_Light_italic_300-Forza_Medium_italic_350.font.js"></script>
-	
-	
 	<script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.nivo.slider.pack.js"></script>
-	
 	<script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.validate.min.js"></script>
-	
-	<!--<script src="<?php bloginfo('template_directory'); ?>/_/js/highlight.js"></script>
-	
-	<script src="<?php bloginfo('template_directory'); ?>/_/js/highlight.jquery.js"></script>
-	<script src="<?php bloginfo('template_directory'); ?>/_/js/languages/ruby.js"></script>
-	<script src="<?php bloginfo('template_directory'); ?>/_/js/languages/python.js"></script>
-	<script src="<?php bloginfo('template_directory'); ?>/_/js/languages/javascript.js"></script>
-	<script src="<?php bloginfo('template_directory'); ?>/_/js/languages/cpp.js"></script>
-	<script src="<?php bloginfo('template_directory'); ?>/_/js/languages/php.js"></script> -->
-
-	
-	<!-- <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" /> -->
 
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
 	<?php wp_head(); ?>
-	
 </head>
 
 <body <?php body_class(); ?>>
