0a84475779bbfc8f4a01446e8e529df88cc1a9a3
Michael Delaney <fusepilot@gmail.com>
2012-01-27 03:03:26 -0600

    fixed articles with no content display in teaser form.

--- wp-content/themes/fusepilot/partials/article_teaser.php
+++ wp-content/themes/fusepilot/partials/article_teaser.php
@@ -5,15 +5,16 @@
     <?php include (TEMPLATEPATH . '/partials/meta.php' ); ?>
   </header>
   
-	<div class="entry-content">
-	  <?php if(get_field('teaser')): ?>
-      <?php $image_id = get_field('teaser');
-      $image_src = wp_get_attachment_image_src($image_id, 'masonry-triple'); ?>
-      <img class="teaser" src="<?php echo $image_src[0]; ?>" />
-    <?php endif; ?>
-	  
-		<?php the_excerpt(); ?>
-		<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>			
-	</div>
+  <?php if(get_field('teaser')): ?>
+    <?php $image_id = get_field('teaser');
+    $image_src = wp_get_attachment_image_src($image_id, 'masonry-triple'); ?>
+    <img class="teaser" src="<?php echo $image_src[0]; ?>" />
+  <?php endif; ?>
+  
+  <?php if(get_content()): ?>
+    <div class="entry-content">
+      <?php the_excerpt(); ?>
+    </div>  
+  <?php endif; ?>
 	
 </article>
