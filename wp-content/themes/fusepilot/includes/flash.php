a88bb08a0a87a3c04334784d92f4ad3583a37583
Michael Delaney <fusepilot@gmail.com>
2012-01-30 09:27:05 -0600

    fixed message placement. shows up when theres no content as well.

--- wp-content/themes/fusepilot/includes/flash.php
+++ wp-content/themes/fusepilot/includes/flash.php
@@ -4,6 +4,11 @@ function set_flash($hash, $content) {
   $_SESSION["flash"][$hash] = $content;
 }
 
+function has_flash() {
+  global $flash;
+  return count($flash) > 0;
+}
+
 function get_flash($hash) {
   global $flash;
   
--- wp-content/themes/fusepilot/partials/article.php
+++ wp-content/themes/fusepilot/partials/article.php
@@ -10,7 +10,6 @@
 <article class="<?php echo $class; ?>">
   
   <?php if(get_field('gallery') || get_field('teaser')): ?>
-
     <div id="gallery">
   		<?php while(the_repeater_field('gallery')): 
   			$thumbnails_id = get_sub_field('image'); 
@@ -22,22 +21,23 @@
   		<?php $image_id = get_field('teaser');
       echo wp_get_attachment_image($image_id, 'gallery'); ?>
     </div>
-
 	<?php endif; ?>
-
-
-  
+	
+	
   <header>
     <h2 class="entry-title"><?php the_title(); ?></h2>
     <?php include (TEMPLATEPATH . '/partials/meta.php' ); ?>
   </header>
 
   <?php if(get_content()): ?>
-  <?php include (TEMPLATEPATH . '/partials/messages.php' );?>
-    
     <div class="entry-content">
+      <?php include (TEMPLATEPATH . '/partials/messages.php' );?>
       <?php the_content(); ?>
-    </div>  
+    </div>
+  <?php elseif(has_flash()): ?>  
+    <div class="entry-content">
+      <?php include (TEMPLATEPATH . '/partials/messages.php' );?>
+    </div>
   <?php endif; ?>
 	
 	<footer>
